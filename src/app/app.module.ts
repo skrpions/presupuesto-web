6f8d501b81d9277fc11d33bd5ec259f45f85f0f7
NESTOR MARTINEZ C <nestor.martinez.c@uniautonoma.edu.co>
2023-03-29 10:36:32 -0500

    refactor: se agregaron los components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './routes/views/home/home.component';
+import { HeaderComponent } from './routes/components/header/header.component';
+import { FormComponent } from './routes/components/form/form.component';
+import { BodyComponent } from './routes/components/body/body.component';
+import { IncomeComponent } from './routes/components/income/income.component';
+import { ExpensesComponent } from './routes/components/expenses/expenses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FormComponent,
+    BodyComponent,
+    IncomeComponent,
+    ExpensesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
